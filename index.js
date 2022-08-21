ad483e50ce12ae4a1629230bb13c5ce63f744cb8
Noah Carralero <nole2kgames@gmail.com>
2022-08-21 17:18:46 -0400

    setup initial folder and sile structure

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const db = require("./config/connections");
+const routes = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(routes);
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.get("*", (req,res) => res.status(404).json({message:"This doesn't seem to exist."}));
+db.once('open', () =>{
+    app.listen(PORT, () => console.log("Your API is now running on server port " + PORT));
+});
